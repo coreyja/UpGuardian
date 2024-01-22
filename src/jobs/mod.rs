e7d436f84a3afbf911869abd653739b89cd3f566
Corey Alexander <coreyja@gmail.com>
2024-01-21 22:32:15 -0500

    Use the new macro

--- src/jobs/mod.rs
+++ src/jobs/mod.rs
@@ -1,26 +1,16 @@
-use cja::jobs::{registry::JobRegistry, worker::JobFromDB, Job};
-
-use crate::AppState;
-
-pub(crate) struct Jobs;
-
 pub(crate) mod create_checkin;
 pub(crate) mod hello;
 
-#[async_trait::async_trait]
-impl JobRegistry<AppState> for Jobs {
-    async fn run_job(&self, job: &JobFromDB, app_state: AppState) -> miette::Result<()> {
-        let payload = job.payload.clone();
+use cja::{
+    impl_job_registry,
+    jobs::{registry::JobRegistry, worker::JobFromDB, Job},
+};
 
-        match job.name.as_str() {
-            "Hello" => hello::Hello::run_from_value(payload, app_state).await,
-            "CreateCheckin" => {
-                create_checkin::CreateCheckin::run_from_value(payload, app_state).await
-            }
-            "BulkEnqueueCheckins" => {
-                create_checkin::BulkEnqueueCheckins::run_from_value(payload, app_state).await
-            }
-            _ => Err(miette::miette!("Unknown job type: {}", job.name)),
-        }
-    }
-}
+use crate::AppState;
+
+impl_job_registry!(
+    AppState,
+    hello::Hello,
+    create_checkin::CreateCheckin,
+    create_checkin::BulkEnqueueCheckins
+);
