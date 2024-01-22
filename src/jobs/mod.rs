2bea9a4fd1517a39005040e41de8672305f2557a
Corey Alexander <coreyja@gmail.com>
2024-01-21 22:39:03 -0500

    Use newest version and cleanup imports

--- src/jobs/mod.rs
+++ src/jobs/mod.rs
@@ -1,15 +1,8 @@
 pub(crate) mod create_checkin;
 pub(crate) mod hello;
 
-use cja::{
-    impl_job_registry,
-    jobs::{registry::JobRegistry, worker::JobFromDB, Job},
-};
-
-use crate::AppState;
-
-impl_job_registry!(
-    AppState,
+cja::impl_job_registry!(
+    crate::AppState,
     hello::Hello,
     create_checkin::CreateCheckin,
     create_checkin::BulkEnqueueCheckins
